0f93c175b76e85d017cdb06e4f42a774541d01ea
justin <justin@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-02-07 20:02:42 +0000

    not using RAILS_ROOT in install.rb
    git-svn-id: https://svn.thinkrelevance.com/streamlined/plugins/streamlined@195 4f249914-c612-0410-8deb-f4485a9d85ab

--- install.rb
+++ install.rb
@@ -1,13 +1,13 @@
 # Install hook code here
 begin
-  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'rico_corner.js'), File.join(RAILS_ROOT, 'public', 'javascripts')
-  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'streamlined.js'), File.join(RAILS_ROOT, 'public', 'javascripts')
-  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'streamlined.rhtml'), File.join(RAILS_ROOT, 'app', 'views', 'layouts')
-  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'streamlined.css'), File.join(RAILS_ROOT, 'public', 'stylesheets')
-  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'overlib'), File.join(RAILS_ROOT, 'public')
-  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'windows_js'), File.join(RAILS_ROOT, 'public')
-  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'grids'), File.join(RAILS_ROOT, 'public', 'stylesheets')
-  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'images'), File.join(RAILS_ROOT, 'public', 'images', 'streamlined')
+  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'rico_corner.js'), File.join(File.dirname(__FILE__), '..','..','..', 'public', 'javascripts')
+  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'streamlined.js'), File.join(File.dirname(__FILE__), '..','..','..', 'public', 'javascripts')
+  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'streamlined.rhtml'), File.join(File.dirname(__FILE__), '..','..','..', 'app', 'views', 'layouts')
+  FileUtils.cp  File.join(File.dirname(__FILE__), 'files', 'streamlined.css'), File.join(File.dirname(__FILE__), '..','..','..', 'public', 'stylesheets')
+  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'overlib'), File.join(File.dirname(__FILE__), '..','..','..', 'public')
+  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'windows_js'), File.join(File.dirname(__FILE__), '..','..','..', 'public')
+  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'grids'), File.join(File.dirname(__FILE__), '..','..','..', 'public', 'stylesheets')
+  FileUtils.cp_r  File.join(File.dirname(__FILE__), 'files', 'images'), File.join(File.dirname(__FILE__), '..','..','..', 'public', 'images', 'streamlined')
 rescue Exception => ex
   puts "FAILED TO COPY FILES DURING STREAMLINED INSTALL.  PLEASE RUN rake streamlined:install_files."
   puts "EXCEPTION: #{ex}"
