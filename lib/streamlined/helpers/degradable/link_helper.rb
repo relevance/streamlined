a0adb796ca43db89ec57ad1362967983b0beba15
stu <stu@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-03-03 21:24:33 +0000

    using link helpers

--- lib/streamlined/helpers/degradable/link_helper.rb
+++ lib/streamlined/helpers/degradable/link_helper.rb
@@ -3,7 +3,7 @@ module Streamlined; module Helpers; end; end
 module Streamlined; module Helpers; module Degradable; end; end; end
 module Streamlined::Helpers::Degradable::LinkHelper
   def link_to_new_model
-    link_to(image_tag('add_16.png', 
+    link_to(image_tag('streamlined/add_16.png', 
         {:alt => "New #{@model_name}", :title => "New #{@model_name}", :border => '0'}),          
         url_for(:action => 'new'))
   end
--- lib/streamlined/helpers/link_helper.rb
+++ lib/streamlined/helpers/link_helper.rb
@@ -2,27 +2,27 @@ module Streamlined; end
 module Streamlined; module Helpers; end; end
 module Streamlined::Helpers::LinkHelper
   def link_to_new_model
-    link_to_function image_tag('add_16.png', 
+    link_to_function image_tag('streamlined/add_16.png', 
         {:alt => "New #{@model_name}", :title => "New #{@model_name}", :border => '0'}),          
         "Streamlined.Windows.open_local_window_from_url('New', '#{url_for(:action => 'new')}')"
   end
   def link_to_xml_export
-    link_to_function(image_tag('export_16.png', 
+    link_to_function(image_tag('streamlined/export_16.png', 
         {:alt => "Export XML", :title => "Export XML", :border => '0'}),
         "Streamlined.Exporter.export_to('#{url_for(:action => 'export_to_xml')}')")    
   end
   def link_to_csv_export
-    link_to_function(image_tag('save_16.png', 
+    link_to_function(image_tag('streamlined/save_16.png', 
         {:alt => "Export CSV", :title => "Export CSV", :border => 0}),
         "Streamlined.Exporter.export_to('#{url_for(:action => 'export_to_csv')}')")
   end
   def link_to_next_page
-    link_to_function image_tag('control-reverse_16.png', 
+    link_to_function image_tag('streamlined/control-reverse_16.png', 
         {:id => 'previous_page', :alt => 'Previous Page', :style => @streamlined_item_pages != [] && @streamlined_item_pages.current.previous ? "" : "display: none;", :title => 'Previous Page', :border => '0'}), 
         "Streamlined.PageOptions.previousPage()"    
   end
   def link_to_previous_page
-    link_to_function image_tag('control-forward_16.png', 
+    link_to_function image_tag('streamlined/control-forward_16.png', 
         {:id => 'next_page', :alt => 'Next Page', :style => @streamlined_item_pages != [] && @streamlined_item_pages.current.next ? "" : "display: none;", :title => 'Next Page', :border => '0'}),   
         "Streamlined.PageOptions.nextPage()"  
   end
