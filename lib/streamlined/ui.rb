167cf75c78b6506802c68b243a0a43fdf18cbce7
stu <stu@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-03-09 18:25:52 +0000

    major renaming

--- lib/streamlined/ui.rb
+++ lib/streamlined/ui.rb
@@ -102,14 +102,14 @@ module Streamlined
        # Given a relationship name, returns the View class representing it.
        def view_def(rel)
          opts = self.relationships[rel.to_sym]
-         Streamlined::Views::EditViews.create_relationship(opts[:view], opts[:view_fields])
+         Streamlined::View::EditViews.create_relationship(opts[:view], opts[:view_fields])
        end
        
        # Given a relationship name, returns the Summary class representing it.
        def summary_def(rel)
          opts = self.relationships[rel.to_sym]
          return nil if opts[:summary] == :none
-         Streamlined::Views::ShowViews.create_summary(opts[:summary], opts[:fields])
+         Streamlined::View::ShowViews.create_summary(opts[:summary], opts[:fields])
        end
        
        # Return list of all known relationships.
@@ -179,7 +179,7 @@ module Streamlined
            if options[:summary] == :none
              @relationships[rel] = Streamlined::Column::Association.new(model.reflect_on_association(rel),nil, nil)
            else
-             @relationships[rel] = Streamlined::Column::Association.new(model.reflect_on_association(rel), Streamlined::Views::EditViews.create_relationship(options[:view][:name], options[:view].reject {|k,v| k == :name}), Streamlined::Views::ShowViews.create_summary(options[:summary][:name], options[:summary].reject {|k,v| k == :name}))         
+             @relationships[rel] = Streamlined::Column::Association.new(model.reflect_on_association(rel), Streamlined::View::EditViews.create_relationship(options[:view][:name], options[:view].reject {|k,v| k == :name}), Streamlined::View::ShowViews.create_summary(options[:summary][:name], options[:summary].reject {|k,v| k == :name}))         
            end
          end
 
