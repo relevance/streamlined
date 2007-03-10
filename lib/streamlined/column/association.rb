2dd92f99f831baac91559d559a8ac650b90b1757
stu <stu@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-03-10 03:10:37 +0000

    humanizing name

--- lib/streamlined/column/association.rb
+++ lib/streamlined/column/association.rb
@@ -5,6 +5,7 @@ class Streamlined::Column::Association
   attr_reader :underlying_association
   attr_reader :view_def
   attr_reader :summary_def
+  attr_accessor :human_name
   
   delegates :name, :to=>:underlying_association
   
@@ -12,6 +13,7 @@ class Streamlined::Column::Association
     @underlying_association = assoc
     @view_def = view
     @summary_def = summary
+    @human_name = name.to_s.humanize
   end
 
   # Returns a list of all the classes that can be used to satisfy this relationship.  In a polymorphic relationship, it is the union 
