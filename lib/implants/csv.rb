77b63d951c79a009708555fb0a1a364445c6d3cd
stu <stu@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-03-20 02:16:07 +0000

    explicit load of csv

--- lib/implants/csv.rb
+++ lib/implants/csv.rb
@@ -1,9 +1,11 @@
+require 'csv'
+
 module Enumerable
   def to_csv(keys, options = {:header=>true})
     str = ''
     header = options.delete(:header)
     header = keys if header == true
-    CSV::Writer.generate(str) do |csv|
+    ::CSV::Writer.generate(str) do |csv|
       csv << header if header
       each do |item|
         csv << keys.inject([]) {|coll,key| coll << item.send(key); coll}
