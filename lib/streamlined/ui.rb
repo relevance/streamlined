9e951854ff9e39d699f81c1fe3838d66510f29ab
justin <justin@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-04-07 02:11:35 +0000

    ui documentation patch from Matthew

--- lib/streamlined/ui.rb
+++ lib/streamlined/ui.rb
@@ -9,9 +9,10 @@ require 'streamlined/reflection'
 
 # Base class for the model-specific declarative UI controller classes.  Each Model class 
 # will have a parallel class in the app/streamlined directory for managing the views.
-# For example, if your application has two models, <tt>User</tt> and <tt>Role</tt> (in <tt>app/models/user.rb</tt> and <tt>role.rb)</tt>, 
-# your Streamlined application would also have <tt>app/streamlined/user.rb</tt> and <tt>role.rb</tt>, containing the classes
-# <tt>UserUI</tt> and <tt>RoleUI</tt>.  
+# For example, if your application has two models, <tt>User</tt> and <tt>Role</tt> (in
+# <tt>app/models/user.rb</tt> and <tt>app/models/role.rb)</tt>, your Streamlined application
+# would also have <tt>app/streamlined/user.rb</tt> and <tt>app/streamlined/role.rb</tt>,
+# containing the classes <tt>UserUI</tt> and <tt>RoleUI</tt>.  
 class Streamlined::UI
   class << self
     include Streamlined::Reflection
@@ -24,25 +25,30 @@ class Streamlined::UI
     declarative_scalar :table_filter, :default=>true
     declarative_scalar :read_only, :default=>false
     
-    def inherited(subclass)
+    def inherited(subclass) #:nodoc:
       # subclasses inherit some settings from superclass
       subclass.table_row_buttons(self.table_row_buttons)
     end      
     
-    # Name of this class minus the "UI" suffix.
+    # Returns the name of this class minus the "UI" suffix.
     def default_model
       raise ArgumentError, "You must set a model" if name.blank?
       Object.const_get(self.name.chomp("UI"))
     end
 
-    # Define the columns that should be visible to the user at runtime.  There 
-    # By default, user_columns excludes:
-    # * any field whose name ends in "_at" (Rails-managed timestamp field)
-    # * any field whose name ends in "_on" (Rails-managed timestamp field)
-    # * any field whose name ends in "_id" (foreign key)
-    # * the "position" field (Rails-managed ordering column)
-    # * the "lock_version" field (Rails-managed optimistic concurrency)
-    # * the "password_hash" field (if using a hashed-password strategy)
+    # Defines the columns that should be visible to the user at runtime.  Takes an array
+    # of column names.  For example:
+    # 
+    #   user_columns :login, :first_name, :last_name
+    # 
+    # Column order is reflected in the view. By default, user_columns excludes:
+    # 
+    # * Any field whose name ends in "_at" (Rails-managed timestamp field)
+    # * Any field whose name ends in "_on" (Rails-managed timestamp field)
+    # * Any field whose name ends in "_id" (foreign key)
+    # * The "position" field (Rails-managed ordering column)
+    # * The "lock_version" field (Rails-managed optimistic concurrency)
+    # * The "password_hash" field (if using a hashed-password strategy)
     def user_columns(*args)
       if args.size > 0
         @user_columns = []
@@ -62,7 +68,7 @@ class Streamlined::UI
       end
     end
 
-    def override_columns(name, *args)
+    def override_columns(name, *args) #:nodoc:
       if args.size > 0
         instance_variable_set(name, [])
         args.each do |arg|
@@ -78,13 +84,30 @@ class Streamlined::UI
         instance_variable_get(name) || user_columns
       end
     end
-
+    
+    # Defines the columns that should be visible when the user clicks the
+    # "Show" button at runtime.  Takes an array of column names.  For example:
+    #
+    #   show_columns :login, :first_name, :last_name
+    #
+    # Column order is reflected in the view. show_columns uses the same
+    # default column exclusions as user_columns.
     def show_columns(*args)
       override_columns(:@show_columns, *args)
     end
+    
+    # Defines the columns that should be editable by the user at runtime.
+    # Takes an array of column names.  For example:
+    #
+    #   edit_columns :login, :first_name, :last_name
+    #
+    # Column order is reflected in the view. edit_columns uses the same
+    # default column exclusions as user_columns.
     def edit_columns(*args)
       override_columns(:@edit_columns, *args)
     end
+    
+    # Alias for user_columns (?)
     def list_columns(*args)
       override_columns(:@list_columns, *args)
     end
@@ -108,11 +131,14 @@ class Streamlined::UI
     def all_columns
       @all_columns ||= (scalars.values + additions.values + relationships.values)
     end
- 
+    
+    # Returns <tt>Streamlined::UI::Generic</tt>, the generic UI class.
     def generic_ui
       Streamlined::UI::Generic
     end
     
+    # Returns the UI class for a given class name. If the named class has no associated
+    # UI class, the generic_ui class will be returned.
     def get_ui(klass_name)
       "#{klass_name}UI".to_const || generic_ui
     end
