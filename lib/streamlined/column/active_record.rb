61f40565d4dc2633c8dd8058a5b59279e5a0254b
matthew <matthew@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-04-16 20:28:04 +0000

    added ability to specify a human name for AR columns

--- lib/streamlined/column/active_record.rb
+++ lib/streamlined/column/active_record.rb
@@ -1,18 +1,19 @@
 class Streamlined::Column::ActiveRecord < Streamlined::Column::Base
-  attr_accessor :ar_column
-  delegates :name, :human_name, :to=>:ar_column
+  attr_accessor :ar_column, :human_name
+  delegates :name, :to=>:ar_column
   def initialize(ar_column)
     @ar_column = ar_column
+    @human_name = ar_column.human_name if ar_column.respond_to?(:human_name)
   end
 
   def ==(o)
     return true if o.object_id == object_id
     return false unless self.class == o.class
-    return self.ar_column == o.ar_column
+    return self.ar_column == o.ar_column &&
+           self.human_name == o.human_name
   end
   
   def render_input(view)
     view.input(view.model_underscore, name)    
   end
-
 end
--- test/unit/streamlined/column/active_record_test.rb
+++ test/unit/streamlined/column/active_record_test.rb
@@ -3,14 +3,34 @@ require 'streamlined/column/addition'
 
 class Streamlined::Column::ActiveRecordTest < Test::Unit::TestCase
   include Streamlined::Column
+  include FlexMock::TestCase
+  
+  def test_names_delegate_to_ar_column
+    ar = ActiveRecord.new(ar_column('foo_bar', 'Foo bar'))
+    assert_equal 'foo_bar', ar.name
+    assert_equal 'Foo bar', ar.human_name
+  end
+  
+  def test_human_name_can_be_set_manually
+    ar = ActiveRecord.new(ar_column('foo_bar', 'Foo bar'))
+    ar.human_name = 'Bar Foo'
+    assert_equal 'Bar Foo', ar.human_name
+  end
   
   def test_equal
-    a1 = ActiveRecord.new(:foo)
+    a1 = ActiveRecord.new(:foo)  
     a2 = ActiveRecord.new(:foo)
     a3 = ActiveRecord.new(:bar)
     a4 = ActiveRecord.new(nil)
     assert_equal a1, a2
     assert_not_equal a1, a3
     assert_not_equal a4, a1
+  end
+  
+  def ar_column(name, human_name)
+    ar_column = flexmock
+    ar_column.should_receive(:name).and_return(name)
+    ar_column.should_receive(:human_name).and_return(human_name)
+    ar_column
   end
 end
