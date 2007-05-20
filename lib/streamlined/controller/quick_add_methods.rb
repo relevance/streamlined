2866639baf0e5f18d53df0ebbc66f46e24f99629
matthew <matthew@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-05-20 18:09:08 +0000

    began refactoring Quick Add actions

--- lib/streamlined/controller/quick_add_methods.rb
+++ lib/streamlined/controller/quick_add_methods.rb
@@ -3,14 +3,11 @@ module Streamlined::Controller::QuickAddMethods
   # TODO: needs refactoring
   def quick_add
     self.crud_context = :new
-    @model_class_name = params[:model_class_name]
-    @model_name = @model_class_name.underscore
-    @model = @model_class_name.constantize.new
-    @ui = Streamlined::UI.get_ui(@model.class)
-    instance_variable_set("@#{@model_name}", @model)
+    set_instance_vars
     @model.class.delegate_targets.each do |dt| 
       assoc = @model.class.reflect_on_association(dt)
       target_class = assoc.class_name.constantize
+      
       instance_variable_set("@#{target_class.name.underscore}", target_class.new)
     end
     self.instance = @model
@@ -19,15 +16,12 @@ module Streamlined::Controller::QuickAddMethods
   
   # TODO: needs refactoring
   def save_quick_add
-    @model_class_name = params[:model_class_name]
-    @model_name = @model_class_name.underscore
-    @model = @model_class_name.constantize.new(params[@model_name.to_sym])
-    @ui = Streamlined::UI.get_ui(@model.class)
-    instance_variable_set("@#{@model_name}", @model)
+    set_instance_vars
     @success = true
     @model.class.delegate_targets.each do |dt| 
       assoc = @model.class.reflect_on_association(dt)
       target_class = assoc.class_name.constantize
+      
       assoc_name = assoc.class_name.underscore.to_sym
       assoc_model = target_class.new(params[assoc_name])
       @success = assoc_model.save && @success
@@ -37,6 +31,15 @@ module Streamlined::Controller::QuickAddMethods
     @success = @model.save && @success
     self.instance = @model
     render_or_redirect(:success, 'save_quick_add')
+  end
+  
+  private
+  def set_instance_vars
+    @model_class_name = params[:model_class_name]
+    @model_name = @model_class_name.underscore
+    @model = @model_class_name.constantize.new(params[@model_name.to_sym])
+    @ui = Streamlined::UI.get_ui(@model.class)
+    instance_variable_set("@#{@model_name}", @model)
   end
  
 end
--- test/unit/streamlined/controller/quick_add_methods_test.rb
+++ test/unit/streamlined/controller/quick_add_methods_test.rb
@@ -8,30 +8,47 @@ class StubClass
   end
 end
 
-class Streamlined::Controller::QuickAddMethodsTest < Test::Unit::TestCase
+class StubController < ActionController::Base
   include Streamlined::Controller::QuickAddMethods
-  attr_accessor :crud_context, :instance
-  
+  attr_accessor :crud_context, :instance, :model, :stub_class,
+                :model_class_name, :model_name, :ui
+end
+
+class Streamlined::Controller::QuickAddMethodsTest < Test::Unit::TestCase
   # TODO: test these actions with a class that uses delegation
+  
+  def setup
+    @controller = StubController.new
+  end
   
   def test_quick_add
     build_param_and_render_mocks('quick_add')
-    quick_add
-    assert_equal :new, crud_context
-    assert instance.is_a?(StubClass)
+    @controller.quick_add
+    assert_equal :new, @controller.crud_context
+    assert_correct_vars_set
   end
   
   def test_save_quick_add
     build_param_and_render_mocks('save_quick_add')
     flexmock(StubClass).new_instances.should_receive(:save).and_return(true).once
-    save_quick_add
-    assert_nil crud_context
-    assert instance.is_a?(StubClass)
+    @controller.save_quick_add
+    assert_nil @controller.crud_context
+    assert_correct_vars_set
+  end
+  
+  def assert_correct_vars_set
+    assert @controller.instance.is_a?(StubClass)
+    assert @controller.model.is_a?(StubClass)
+    assert @controller.stub_class.is_a?(StubClass)
+    
+    assert_equal 'StubClass', @controller.model_class_name
+    assert_equal 'stub_class', @controller.model_name    
+    assert_equal Streamlined::UI::Generic, @controller.ui
   end
   
   def build_param_and_render_mocks(template)
-    flexmock(self) do |mock|
-      mock.should_receive(:params => { :model_class_name => 'StubClass' })
+    flexmock(@controller) do |mock|
+      mock.should_receive(:params => { :model_class_name => 'StubClass' }).at_least.once
       mock.should_receive(:render_or_redirect).with(:success, template).once
     end
   end
