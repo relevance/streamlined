320302618743ad6a9b107374d6cfc08faed0e12a
jason <jason@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-06-11 17:56:49 +0000

    Added socket declaration necessary to establish DB connection for tests

--- test/ar_helper.rb
+++ test/ar_helper.rb
@@ -5,7 +5,8 @@ ActiveRecord::Base.configurations = {
   'streamlined_unittest' => {
     :adapter => 'mysql',
     :username => 'root',
-    :database => 'streamlined_unittest'
+    :database => 'streamlined_unittest',
+    :socket => '/tmp/mysql.sock'
   },
 }
 ActiveRecord::Base.establish_connection 'streamlined_unittest'
