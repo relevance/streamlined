773fe296f4437050d148d5eb45bea7ee0c78a6ec
rob <rob@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-07-30 18:56:21 +0000

    add more docs for side and top menus

--- lib/streamlined/helpers/layout_helper.rb
+++ lib/streamlined/helpers/layout_helper.rb
@@ -1,12 +1,23 @@
 # These helpers are needed by the layout
 # You might need to include them in non-streamlined controllers that want to share layout
 module Streamlined::Helpers::LayoutHelper
-  # override these in your own application helper, or controller specific helpers
+
+  # Returns an array of arrays which define the side menus for the corresponding view.
+  # Override all side menus in all Streamlined controllers by defining this method in
+  # ApplicationHelper, or override on a controller by controller basis in their specific
+  # helpers.
+  # Each array should contain the text of the link and the URL parameters for the link.
   def streamlined_side_menus
     [
       ["TBD", {:action=>"list"}]
     ]
   end
+
+  # Returns an array of arrays which define the top menus for the corresponding view.
+  # Override all top menus in all Streamlined controllers by defining this method in
+  # ApplicationHelper, or override on a controller by controller basis in their specific
+  # helpers.
+  # Each array should contain the text of the link and the URL parameters for the link.
   def streamlined_top_menus
     [
       ["TBD", {:action=>"new"}]
