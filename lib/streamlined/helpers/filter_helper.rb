07b224cfe1c4767f6236e15b02162b9334db5d4e
kerry <kerry@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-08-02 19:00:32 +0000

    Enhancement to Advanced Filtering.  For association columns, filter on fields defined in the show_view of the association in the UI file.  If there is no show view use the defaults of name and title.

--- lib/streamlined/helpers/filter_helper.rb
+++ lib/streamlined/helpers/filter_helper.rb
@@ -10,14 +10,25 @@ module Streamlined::Helpers::FilterHelper
         filter_columns[column.human_name] = column.name 
       elsif column.is_a?(Streamlined::Column::Association)
         association_name = column.name
-        names = %w{name title}
-        no_name_yet = true
-        names.each do |name|
-          if no_name_yet && model.reflect_on_association(association_name).klass.column_names.index(name) 
-            filter_columns[Inflector.humanize(association_name.to_s) + " (#{name})"] = "rel::" + association_name.to_s + "::" + "#{name}"
-            no_name_yet = false
+        # If there are fields defined for the show_view in the ModelUI file then use those
+        # otherwise look for a name or title on the association and filter on that.
+        # Checking against column_names ensures that they are db fields and not defines in the model.
+        if model_ui.relationships[association_name].show_view.fields
+          model_ui.relationships[association_name].show_view.fields.each do |field|
+            if model.reflect_on_association(association_name).klass.column_names.index(field.to_s) 
+              filter_columns[Inflector.humanize(association_name.to_s) + " (" + Inflector.humanize(field) + ")"] = "rel::" + association_name.to_s + "::" + "#{field}"
+            end
           end
-        end    
+        else  
+          names = %w{name title}
+          no_name_yet = true
+          names.each do |name|
+            if no_name_yet && model.reflect_on_association(association_name).klass.column_names.index(name) 
+              filter_columns[Inflector.humanize(association_name.to_s) + " (" + Inflector.humanize(name) + ")"] = "rel::" + association_name.to_s + "::" + "#{name}"
+              no_name_yet = false
+            end
+          end    
+        end
       end  
     end
     filter_columns.sort 
--- test/functional/streamlined/helpers/filter_helper_functional_test.rb
+++ test/functional/streamlined/helpers/filter_helper_functional_test.rb
@@ -22,7 +22,7 @@ class Streamlined::Helpers::FilterHelperFunctionalTest < Test::Unit::TestCase
     author_columns = AuthorUI.list_columns
     assert_equal 6, author_columns = AuthorUI.list_columns.length
     assert_equal 4, advanced_filter_columns.length
-    assert_equal [["Articles (title)", "rel::articles::title"],["Books (title)", "rel::books::title"],["First name", "first_name"],["Last name", "last_name"]], advanced_filter_columns
+    assert_equal [["Articles (Title)", "rel::articles::title"],["Books (Title)", "rel::books::title"],["First name", "first_name"],["Last name", "last_name"]], advanced_filter_columns
   end
   
   def complex_controller_and_view
@@ -36,4 +36,26 @@ class Streamlined::Helpers::FilterHelperFunctionalTest < Test::Unit::TestCase
     get 'index'
     @view = @response.template
   end
+  
+  # Check that relation columns Authors::last_name and Authors::first_name get included for filtering
+  # and that Authors::full_name does not since it is not a db field, just a define in Author.rb 
+  def test_advanced_filter_columns_with_fields
+    @controller = ArticlesController.new
+    @controller.logger = RAILS_DEFAULT_LOGGER
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    @item = Struct.new(:id).new(1)
+    get 'index'
+    @view = @response.template
+    @article_ui = Streamlined.ui_for(Article)
+    
+    advanced_filter_columns = @view.advanced_filter_columns
+    article_columns = @article_ui.list_columns
+    assert_equal 2, article_columns = @article_ui.user_columns.length, "Should only have 2 list_columns in ArticlesUI file"
+    assert_equal 3, @article_ui.relationships[:authors].show_view.fields.length, "Testing that there are 3 fields of which 2 will be picked.  Check for changes in ArticlesUI file."
+    assert_equal [:first_name, :last_name, :full_name], @article_ui.relationships[:authors].show_view.fields, "Testing that there are 3 fields of which 2 will be picked.  Check for changes in ArticlesUI file."
+    assert_equal 3, advanced_filter_columns.length, "Should have 3 columns to filter on Author-last_name, Author-first_name and Title"
+    assert_equal [["Authors (First name)", "rel::authors::first_name"],["Authors (Last name)", "rel::authors::last_name"],["Title", "title"]], advanced_filter_columns, "Should have 3 columns to filter on Author-last_name, Author-first_name and Title"
+  end
+  
 end
