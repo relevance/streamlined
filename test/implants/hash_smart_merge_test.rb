5d08ac21d7ba600f5c713ff0d2e6542370f36ee3
matthew <matthew@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-09-13 18:51:08 +0000

    added Hash#smart_merge! for more intelligent merging of filter/order options in the CRUD actions

---
+++ test/implants/hash_smart_merge_test.rb
@@ -0,0 +1,39 @@
+require File.join(File.dirname(__FILE__), '../test_helper')
+
+class HashSmartMergeTest < Test::Unit::TestCase
+  
+  def test_smart_merge!
+    one = { :foo => "123", :bar => "456" }
+    two = { :bar => "789", :bat => "012" }
+    one.smart_merge!(two)
+    expected = { :foo => "123", :bar => ["456", "789"], :bat => "012" }
+    assert_equal expected, one
+  end
+  
+  def test_smart_merge_with_nils
+    one = { :foo => "123", :bar => nil }
+    two = { :bar => "789", :bat => "012" }
+    one.smart_merge!(two)
+    expected = { :foo => "123", :bar => [nil, "789"], :bat => "012" }
+    assert_equal expected, one
+  end
+  
+  def test_smart_merge_with_an_array_value
+    one = { :foo => "123", :bar => ["566", "667"] }
+    two = { :bar => "789", :bat => "012" }
+    one.smart_merge!(two)
+    expected = { :foo => "123", :bar => [["566", "667"], "789"], :bat => "012" }
+    assert_equal expected, one
+  end
+  
+  def test_smart_merge_three_hashes
+    one = { :foo => "123", :bar => "456" }
+    two = { :bar => "789", :bat => "012" }
+    thr = { :bat => "556", :ant => "667" }
+    one.smart_merge!(two)
+    one.smart_merge!(thr)
+    expected = { :foo => "123", :bar => ["456", "789"], :bat => ["012", "556"], :ant => "667" }
+    assert_equal expected, one
+  end
+  
+end
