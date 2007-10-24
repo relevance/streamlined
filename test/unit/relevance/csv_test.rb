4599dd0255a740b58940ab386b513497f55ea0ac
justin <justin@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-10-24 14:53:46 +0000

    breaking the build

--- test/unit/relevance/csv_test.rb
+++ test/unit/relevance/csv_test.rb
@@ -25,5 +25,9 @@ class EnumerableTest < Test::Unit::TestCase
   def test_boolean_header_with_different_separator
     assert_equal("upcase;to_str\nA;a\nB;b\n", ['a','b'].to_csv([:upcase, :to_str], {:header=>true, :separator=>";"} ))
   end
+  
+  def test_breaking_the_build
+    raise "OH YEAH"
+  end
 
 end
