9feb6ff9c3bc723c648d0fa689ff7c78891a9006
rob <rob@4f249914-c612-0410-8deb-f4485a9d85ab>
2007-10-26 23:02:59 +0000

    blow away multi rails, since i think i hosed it

--- test/multi_rails/init.rb
+++
@@ -1,4 +0,0 @@
-if Object.const_defined?("RAILS_ENV") && RAILS_ENV == "test"
-  require File.expand_path(File.join(File.dirname(__FILE__), "lib/multi_rails"))
-  MultiRails::gem_and_require_rails
-end
